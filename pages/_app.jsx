1bc8534db1942ce4686fcd442bb3df82ecff5e25
sagar <ahiresagar06130@gmail.com>
2024-03-23 20:13:05 +0530

    regarding amplitude integration.

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import amplitude from 'amplitude-js';
+
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRefresh = () => {
+
+      amplitude.getInstance().logEvent('Page Refresh');
+    };
+
+    window.addEventListener('beforeunload', handleRefresh);
+
+    return () => {
+      window.removeEventListener('beforeunload', handleRefresh);
+    };
+  }, []);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
